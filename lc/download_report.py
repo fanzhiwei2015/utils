aaca7aece390b1309b5f36eaa9073a1ab471caa4
fanzhiwei <fanzhiwei2015>
2020-04-25 23:43:04 +0800

    add es

--- lc/download_report.py
+++ lc/download_report.py
@@ -2,30 +2,72 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 import os
-url="https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001409970&type=424B3&dateb=&owner=exclude&count=40"
+import traceback
+from parse_report import Parse_report
+import sys
+import time
+
+start=0
+if len(sys.argv) > 0:
+    start=sys.argv[1]
+url="https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001409970&type=424B3&dateb=&owner=exclude&start=%s&count=100" % start
+print(url)
 sec_base_url="https://www.sec.gov"
+if not os.path.isdir("archives"):
+    os.mkdir("archives")
 
 result=requests.get(url)
 soup=bs(result.text, 'html.parser')
 links=soup.findAll('a', attrs={'href': re.compile("Archives/edgar/data")})
 checked_links_file="checked_links.txt"
+checked_links=[]
 if os.path.exists(checked_links_file):
     f_checked_links=open(checked_links_file,"r")
-    checked_links = f_checked_links.readlines()
+    for line in f_checked_links:
+        checked_links.append(line.strip())
     f_checked_links.close()
-else:
-    checked_links=[]
+
 
 f_checked_links=open(checked_links_file,"a+")
 for link in links:
+    time.sleep(0.5)
     link_href=link.attrs["href"]
-    import pdb;pdb.set_trace()
+    #import pdb;pdb.set_trace()
     if link_href in checked_links:
         continue
     else:
         doc_url=sec_base_url + link_href
-        f_checked_links.write(link_href + "\n")
-        print(doc_url)
+        print("opening document link %s " % doc_url)
+        doc_result=requests.get(doc_url)
+        doc_soup = bs(doc_result.text, 'html.parser')
+        doc_links = doc_soup.findAll('a', attrs={'href': re.compile("/Archives/.*htm")})
+        try:
+            for doc_link in doc_links:
+                doc_link_href=doc_link.attrs["href"]
+                base_doc_link=doc_link_href.split("/")[-1]
+                doc_link_url=sec_base_url + doc_link_href
+
+                if not os.path.exists("archives/%s" % base_doc_link):
+                    doc_link_result = requests.get(doc_link_url)
+                    with open('archives/%s' % base_doc_link, 'wb') as f:
+                        f.write(doc_link_result.content)
+                #import pdb;pdb.set_trace()
+
+                report = Parse_report("archives/%s" % base_doc_link )
+                if "postsup" in base_doc_link:
+                    report.run_parse_post()
+                elif "salessup" in base_doc_link:
+                    report.run_parse_sale()
+                report.insert_es()
+                f_checked_links.write(link_href + "\n")
+                #import pdb;pdb.set_trace()
+        except:
+            traceback.print_exc()
+            #print(tb)
+        #import pdb;pdb.set_trace()
+
+
+
 
 f_checked_links.close()
-import pdb;pdb.set_trace()
+#import pdb;pdb.set_trace()
--- lc/parse_report.py
+++ lc/parse_report.py
@@ -1,124 +1,269 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 import os
 import sys
 from bs4 import BeautifulSoup as bs
 import datetime
-# to form the output as below data
-# results=[{"loadID","168822424","loanAmount":1000,"interestRate":0.1033,"ServiceCharge":0.01,"initialMaturity","Five years after issurance","Finalmaturity":"Five years after inssurance"..]
-#
-#
-filename=sys.argv[1]
-print("parsing report %s" % filename)
-def month_to_number(m):
-	mapping={
-		"January":1,
-		"February":2,
-		"March":3,
-		"April":4,
-		"May":5,
-		"June": 6,
-		"July": 7,
-		"August": 8,
-		"September": 9,
-		"Octor": 10,
-		"November": 11,
-		"December": 12,
-		}
-	return mapping[m]
+import json
+from time import sleep
+
+# use the Elasticsearch client's helpers class for _bulk API
+from elasticsearch import Elasticsearch, helpers
+
+# declare a client instance of the Python Elasticsearch library
+client = Elasticsearch("localhost:9200")
+
+# def month_to_number(m):
+# 	mapping={
+# 		"January":1,
+# 		"February":2,
+# 		"March":3,
+# 		"April":4,
+# 		"May":5,
+# 		"June": 6,
+# 		"July": 7,
+# 		"August": 8,
+# 		"September": 9,
+# 		"Octor": 10,
+# 		"November": 11,
+# 		"December": 12,
+# 		}
+# 	if m not in mapping:
+# 		import pdb;pdb.set_trace()
+# 	return mapping[m]
 
 def get_request_date(line):
 	lines=line.split(" ")
 	loanID=lines[2]
-	rdate_y=str(lines[8])
-	rdate_m=lines[6]
-	rdate_m=str(month_to_number(rdate_m))
-	rdate_d=str(lines[7].replace(",",""))
-	rdate="-".join([rdate_y,rdate_m,rdate_d])
+	rdate="%s %s %s" % (lines[6],lines[7], lines[8])
 	return (loanID,rdate)
 	
-infile=open(filename,"r")
-pages=[]
-cur=0
-pages.append("")
-request_date={}
-credit_report_date={}
 
-for line in infile:
-	if "<HR " in line:
-		cur += 1
+class Parse_report:
+	def __init__(self, report):
+		self.report = report
+		self.loansinfo = []
+
+	def insert_es(self):
+		resp = helpers.bulk(
+			client,
+			self.loansinfo,
+			index="lc-%s" % datetime.datetime.now().strftime("%Y-%m-%d"),
+			doc_type="_doc"
+		)
+
+		# print the response returned by Elasticsearch
+		print("helpers.bulk() RESPONSE:", resp)
+		print("helpers.bulk() RESPONSE:", json.dumps(resp, indent=4))
+
+	def run_parse_post(self):
+		pages=[]
+		cur=0
 		pages.append("")
-		continue
-	elif "by a borrower with the following characteristics" in line:
-		#print(line)
-		loanID,rdate=get_request_date(line)
-		request_date.update({loanID:rdate})	
-	elif "A credit bureau reported the following information" in line:
-		#crdate=get_cr_date(line)
-		pass
-	else:
-		pages[cur] += line
-loanTable_Index=1
-for page in pages:
-	soup=bs(page,'html.parser')
-	tables=soup.find_all('table')
-
-	#table 1 loan information
-	rows=tables[loanTable_Index].find_all('tr')
-	columns_header= rows[0].find_all('td')
-	columns_data = rows[1].find_all('td')
-	loaninfo={}
-	cur=0
-	while cur < len(columns_header):
-		key=columns_header[cur].get_text().strip().replace(" ","_").replace("\n","_")
-		value = columns_data[cur].get_text().strip()
-		if value[0] == "$":
-			value = int(value.replace("$","").replace(",",""))
-		elif value[-1] == "%":
-			value = round(float(value.replace("%",""))/100,2)
-		loaninfo[key]=value
-		cur += 1
-	#print(loaninfo)
-	if loaninfo["Member_Loan_ID"] in request_date:
-		rdate = request_date[loaninfo["Member_Loan_ID"]]
-	else:
-		rdate = datetime.datetime.now().strftime("%Y-%m-%d")
-	loaninfo.update({"request_date": rdate})
-	print(loaninfo)
-
-
-	#table2 borrow information
-	rows=tables[loanTable_Index+1].find_all('tr')
-	for row in rows:
-		columns = row.find_all('td')
-		cur=0
-		while cur < len(columns):
+		request_date={}
+		credit_report_date={}
+		#import pdb;pdb.set_trace()
+		infile = open(self.report, "r")
+		for line in infile:
+			if "<HR " in line:
+				cur += 1
+				pages.append("")
+				continue
+			elif "by a borrower with the following characteristics" in line:
+				#print(line)
+				loanID,rdate=get_request_date(line)
+				request_date.update({loanID:rdate})
+			elif "A credit bureau reported the following information" in line:
+				#crdate=get_cr_date(line)
+				pass
+			else:
+				pages[cur] += line
+		loanTable_Index=1
+		for page in pages:
 			#import pdb;pdb.set_trace()
-			key = columns[cur].get_text().strip().replace(" ","_").replace("\n", "_")
-			value = columns[cur+1].get_text().strip()
-			if value[0] == "$":
-				value = int(value.split("/")[0].replace("$","").replace(",",""))
-			elif value[-1] == "%":
-				value = round(float(value.replace("%",""))/100,2)
-			loaninfo[key]=value
-			cur += 2
-		print(loaninfo)
+			soup=bs(page,'html.parser')
+			tables=soup.find_all('table')
 
-	#table3 borrow credit
-	rows=tables[loanTable_Index+2].find_all('tr')
-	for row in rows:
-		columns = row.find_all('td')
-		cur=0
-		while cur < len(columns):
+			#table 1 loan information
+			rows=tables[loanTable_Index].find_all('tr')
+			columns_header= rows[0].find_all('td')
+			columns_data = rows[1].find_all('td')
+			loaninfo={}
+			cur=0
+			while cur < len(columns_header):
+				key=columns_header[cur].get_text().strip().replace(" ","_").replace("\n","_").replace(":","")
+				value = columns_data[cur].get_text().strip()
+				if value[0] == "$":
+					value = int(value.replace("$","").replace(",",""))
+				elif value[-1] == "%":
+					value = round(float(value.replace("%",""))/100,2)
+				loaninfo[key]=value
+				cur += 1
+			#print(loaninfo)
+			if loaninfo["Member_Loan_ID"] in request_date:
+				rdate = request_date[loaninfo["Member_Loan_ID"]]
+			else:
+				rdate = datetime.datetime.now().strftime("%B %d, %Y")
 			#import pdb;pdb.set_trace()
-			key = columns[cur].get_text().strip().replace(" ","_").replace("\n", "_")
-			value = columns[cur+1].get_text().strip()
-			if value[0] == "$":
-				value = int(value.replace("$","").replace(",","").split(".")[0])
-			elif value[-1] == "%":
-				value = round(float(value.replace("%",""))/100,2)
-			elif value.isdigit():
-				value=int(value)
-			loaninfo[key]=value
-			cur += 2
-		print(loaninfo)
-	loanTable_Index = 0
+			loaninfo.update({"request_date": datetime.datetime.strptime(rdate,"%B %d, %Y")})
 
+			es_info={"_id": loaninfo["Member_Loan_ID"],
+					 "timestamp": datetime.datetime.strptime(rdate,"%B %d, %Y"),
+					 "report_type": "post"}
+			loaninfo.update(es_info)
+
+			#print(loaninfo)
+
+
+			#table2 borrow information
+			rows=tables[loanTable_Index+1].find_all('tr')
+			for row in rows:
+				columns = row.find_all('td')
+				cur=0
+				while cur < len(columns):
+					#import pdb;pdb.set_trace()
+					key = columns[cur].get_text().strip().replace(" ","_").replace("\n", "_").replace(":","")
+					value = columns[cur+1].get_text().strip()
+					if value[0] == "$":
+						value = int(value.split("/")[0].replace("$","").replace(",",""))
+					elif value[-1] == "%":
+						value = round(float(value.replace("%",""))/100,2)
+					elif value.isdigit():
+						value=int(value)
+					if "n/a" not in str(value).lower():
+						loaninfo[key]=value
+					cur += 2
+				#print(loaninfo)
+
+			#table3 borrow credit
+			rows=tables[loanTable_Index+2].find_all('tr')
+			for row in rows:
+				columns = row.find_all('td')
+				cur=0
+				while cur < len(columns):
+					#import pdb;pdb.set_trace()
+					key = columns[cur].get_text().strip().replace(" ","_").replace("\n", "_").replace(":","")
+					value = columns[cur+1].get_text().strip()
+					if value[0] == "$":
+						value = int(value.replace("$","").replace(",","").split(".")[0])
+					elif value[-1] == "%":
+						value = round(float(value.replace("%",""))/100,2)
+					elif value.isdigit():
+						value=int(value)
+
+					if "n/a" not in str(value).lower():
+						loaninfo[key]=value
+					cur += 2
+				#print(loaninfo)
+
+			self.loansinfo.append(loaninfo)
+			print("post", loaninfo["Member_Loan_ID"],"added")
+			loanTable_Index = 0
+
+	def run_parse_sale(self):
+		pages=[]
+		cur=0
+		pages.append("")
+		request_date={}
+		credit_report_date={}
+		#import pdb;pdb.set_trace()
+		infile = open(self.report, "r")
+		for line in infile:
+			if "<HR " in line:
+				cur += 1
+				pages.append("")
+				continue
+
+			else:
+				pages[cur] += line
+		loanTable_Index=1
+		for page in pages:
+			#import pdb;pdb.set_trace()
+			soup=bs(page,'html.parser')
+			tables=soup.find_all('table')
+
+			#table 1 loan information
+			rows=tables[loanTable_Index].find_all('tr')
+			columns_header= rows[0].find_all('td')
+			columns_data = rows[1].find_all('td')
+			loaninfo={}
+			cur=0
+			while cur < len(columns_header):
+				key=columns_header[cur].get_text().strip().replace(" ","_").replace("\n","_").replace(":","")
+				value = columns_data[cur].get_text().strip()
+				if value[0] == "$":
+					value = int(value.replace("$","").replace(",",""))
+				elif value[-1] == "%":
+					value = round(float(value.replace("%",""))/100,2)
+				elif "Issue_Date" in key or "maturity" in key:
+					value = datetime.datetime.strptime(value, "%B %d, %Y")
+				loaninfo[key]=value
+				cur += 1
+			es_info={"_id": loaninfo["Series_of_Member_Payment_Dependent_Notes"],
+					 "timestamp": loaninfo["Sale_and_Original_Issue_Date"],
+					 "report_type": "sales"}
+			loaninfo.update(es_info)
+
+			#print(loaninfo)
+
+
+			#table2 borrow information
+			rows=tables[loanTable_Index+1].find_all('tr')
+			for row in rows:
+				columns = row.find_all('td')
+				cur=0
+				while cur < len(columns):
+					#import pdb;pdb.set_trace()
+					key = columns[cur].get_text().strip().replace(" ","_").replace("\n", "_").replace(":","")
+					value = columns[cur+1].get_text().strip()
+					#import pdb;pdb.set_trace()
+					if key=="":
+						cur +=2
+						continue
+					if value[0] == "$":
+						value = int(value.split("/")[0].replace("$","").replace(",",""))
+					elif value[-1] == "%":
+						value = round(float(value.replace("%",""))/100,2)
+					elif value.isdigit():
+						value=int(value)
+					if "n/a" not in str(value).lower():
+						loaninfo[key]=value
+					cur += 2
+				#print(loaninfo)
+
+			#table3 borrow credit
+			rows=tables[loanTable_Index+2].find_all('tr')
+			for row in rows:
+				columns = row.find_all('td')
+				cur=0
+				while cur < len(columns):
+					#import pdb;pdb.set_trace()
+					key = columns[cur].get_text().strip().replace(" ","_").replace("\n", "_").replace(":","")
+					value = columns[cur+1].get_text().strip()
+
+					if key=="":
+						cur +=2
+						continue
+					if value[0] == "$":
+						value = int(value.replace("$","").replace(",","").split(".")[0])
+					elif value[-1] == "%":
+						value = round(float(value.replace("%",""))/100,2)
+					elif value.isdigit():
+						value=int(value)
+
+					if "n/a" not in str(value).lower():
+						loaninfo[key]=value
+					cur += 2
+				#print(loaninfo)
+
+			self.loansinfo.append(loaninfo)
+			print("sales", loaninfo["Series_of_Member_Payment_Dependent_Notes"],"added")
+			loanTable_Index = 0
+
+
+if __name__ == "__main__":
+	filename = sys.argv[1]
+	print("parsing report %s" % filename)
+
+	report=Parse_report(filename)
+	report.run_parse()
